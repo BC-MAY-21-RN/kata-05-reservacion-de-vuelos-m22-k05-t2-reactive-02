8fc8deb758c8f0708cfb3be2e1fbe4f29d260e09
Samuel <samuel16mendoza@gmail.com>
2022-06-13 23:39:48 -0500

    components was created
    The components was added to the FromScreen, add listSelect to indicate
    where is the country where the user is ubicated.

---
+++ ReservacionDeVuelos/src/components/atoms/TextIndicator/style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+    marginTop: 220,
+    fontWeight: 'bold',
+    color: 'black',
+    marginHorizontal: 30,
+  },
+});
+
+export default styles;
--- ReservacionDeVuelos/src/screens/FromScreen/index.js
+++ ReservacionDeVuelos/src/screens/FromScreen/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, TouchableOpacity, Text, SafeAreaView} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import TextIndicator from '../../components/atoms/TextIndicator';
+import styles from './styles';
 
 const FromScreen = () => {
   return (
     <View>
-      <Text>Hola</Text>
+      <TextIndicator text={'Where are you now?'} />
+      <View style={styles.containerList}>
+        <SafeAreaView style={styles.container}>
+          <RNPickerSelect
+            placeholder={{label: 'Select your location', value: null}}
+            onValueChange={value => console.log(value)}
+            items={[
+              {label: 'Belgrade, Serbia', value: 'BEG'},
+              {label: 'Berlin, Germany', value: 'BEL'},
+              {label: 'Oaxaca, México', value: 'OAX'},
+            ]}
+          />
+        </SafeAreaView>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 };
