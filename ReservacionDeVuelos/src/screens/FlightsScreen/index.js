d7110c8a20e8f3a5114f671d916da6de0fe2c59e
unknown <dag_98@hotmail.com>
2022-06-16 18:45:24 -0500

    add necessary code
    Necessary code was added so that props can be passed
    
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>
    Co-authored-by: LizyMiGo <jmiguel@ucol.mx>
    Co-authored-by: ShamuXX <samuel16mendoza@gmail.com>01~

--- ReservacionDeVuelos/src/screens/FlightsScreen/index.js
+++ ReservacionDeVuelos/src/screens/FlightsScreen/index.js
@@ -4,6 +4,15 @@ import CardFlight from '../../components/molecules/CardFlight';
 import auth from '@react-native-firebase/auth';
 import AddButton from '../../components/atoms/AddButton';
 
+const flights = [
+  {
+    from: {title: 'AWS', subtitle: 'Amazon web services'},
+    to: {title: 'ML', subtitle: 'Mercado Libre'},
+    date: '2022-06-16',
+    passengers: 5,
+  },
+];
+
 const salir = () => {
   auth()
     .signOut()
@@ -13,7 +22,7 @@ const salir = () => {
 export default function FlightsScreen({navigation}) {
   return (
     <View style={{height: '100%', width: '100%'}}>
-      <CardFlight />
+      <CardFlight valueFlight={flights[0]} />
       <TouchableOpacity onPress={() => salir()}>
         <Text>Salir</Text>
       </TouchableOpacity>
