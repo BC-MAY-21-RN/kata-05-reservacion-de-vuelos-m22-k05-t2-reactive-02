89ba980e1612b38165fc9a8734e3d37dd5f46fc3
unknown <dag_98@hotmail.com>
2022-06-17 16:20:05 -0500

    fix login screen
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>
    Co-authored-by: LizyMiGo <jmiguel@ucol.mx>
    Co-authored-by: ShamuXX <samuel16mendoza@gmail.com>

--- ReservacionDeVuelos/src/models/LoginUser.js
+++ ReservacionDeVuelos/src/models/LoginUser.js
@@ -1,23 +1,23 @@
 class LoginUser {
   constructor(input1 = '', input2 = '') {
-    this.valuesRegister = {
+    this.valuesLogin = {
       input1: input1,
       input2: input2,
     };
   }
 
   getValues() {
-    return this.valuesRegister;
+    return this.valuesLogin;
   }
 
   setValues(value) {
-    this.valuesRegister = {...this.valuesRegister, ...value};
+    this.valuesLogin = {...this.valuesLogin, ...value};
   }
 
   getBool() {
     if (
-      this.valuesRegister.input1.length > 0 &&
-      this.valuesRegister.input2.length > 0
+      this.valuesLogin.input1.length > 0 &&
+      this.valuesLogin.input2.length > 0
     ) {
       return true;
     } else {
--- ReservacionDeVuelos/src/screens/LoginScreen/index.js
+++ ReservacionDeVuelos/src/screens/LoginScreen/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import texts from '../../consts/text';
 import styles from './styles';
 import InputComponent from '../../components/atoms/InputComponent';
 import Buttons from '../../components/atoms/Buttons';
 import BottomText from '../../components/atoms/BottomText';
+import LoginUser from '../../models/LoginUser';
+
+const newObject = object => {
+  return new LoginUser(object.valuesLogin.input1, object.valuesLogin.input2);
+};
 
 export default function LoginScreen({navigation}) {
+  const [form, setform] = useState(new LoginUser());
+
+  const changeForm = (value, key) => {
+    form.setValues({[key]: value});
+    setform(newObject(form));
+  };
+
   return (
     <View>
       <Text style={styles.title}>{texts.login.title}</Text>
-      <InputComponent title={'Email'} input={'input1'} alert={false} />
-      <InputComponent title={'Password'} input={'input2'} alert={false} />
-      <Buttons />
+      <InputComponent
+        title={'Email'}
+        input={'input1'}
+        alert={false}
+        changeForm={changeForm}
+        text={form.getValues().input1.length > 0}
+      />
+      <InputComponent
+        title={'Password'}
+        input={'input2'}
+        alert={false}
+        changeForm={changeForm}
+      />
+      <Buttons buttonsActive={form.getBool()} />
       <BottomText
         navigation={navigation}
         text={'Sign Up'}
