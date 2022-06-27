8db13e9b20d7d7caf9bae5596535f459c2107748
unknown <dag_98@hotmail.com>
2022-06-27 16:40:04 -0500

    fix screens code
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>

--- ReservacionDeVuelos/src/screens/FlightsScreen/index.js
+++ ReservacionDeVuelos/src/screens/FlightsScreen/index.js
@@ -4,6 +4,15 @@ import CardFlight from '../../components/molecules/CardFlight';
 import auth from '@react-native-firebase/auth';
 import AddButton from '../../components/atoms/AddButton';
 
+/*import React, {useCallback, useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import CardFlight from '../../components/molecules/CardFlight';
+import auth from '@react-native-firebase/auth';
+import AddButton from '../../components/atoms/AddButton';
+import firestore from '@react-native-firebase/firestore';
+import UserContext from '../../context/UserContext';
+import {useFocusEffect} from '@react-navigation/native';*/
+
 const flights = [
   {
     from: {title: 'AWS', subtitle: 'Amazon web services'},
@@ -22,7 +31,9 @@ const salir = () => {
 export default function FlightsScreen({navigation}) {
   return (
     <View style={{height: '100%', width: '100%'}}>
-      <CardFlight valueFlight={flights[0]} />
+      {flights.map((item, index) => (
+        <CardFlight valueFlight={item} />
+      ))}
       <TouchableOpacity onPress={() => salir()}>
         <Text>Salir</Text>
       </TouchableOpacity>
